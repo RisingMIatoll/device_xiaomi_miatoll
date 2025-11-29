f62e3e111b8945a924167fc2da68520410c436e3
AbdoAhmedElbanaa <115952879+AbdoAhmedElbanaa@users.noreply.github.com>
2025-11-29 15:59:40 +0200

    Update MiuiCamera include path in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,7 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 TARGET_USES_ION := true
 
 # MiuiCamera
--include vendor/xiaomi/miuicamera/BoardConfigMiuiCamera.mk
+-include vendor/xiaomi/miuicamera-miatoll/BoardConfigMiuiCamera.mk
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := curtana,excalibur,gram,joyeuse,miatoll
